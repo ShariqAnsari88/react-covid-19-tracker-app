788a083b5d60b9481dee29ede0d759ef4c7eb179
Mohammed Shariq <mohammed.shariq@iris-worldwide.com>
2020-04-06 23:35:05 +0530

    covid tracker app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Cards, Chart, CountryPicker } from './Components/';
+import fetchData from './Api';
+
+import styles from './app.module.scss';
+
+export default class app extends Component {
+
+    state = {
+        data: {},
+    }
+
+    async componentDidMount() {
+        const fetcheData = await fetchData();
+
+        this.setState({ data: fetcheData },()=>{
+            //console.log(this.state.data);
+        })
+    }
+    render() {
+
+        const { data } = this.state
+
+        
+
+        return (
+            <div className={styles.container}>
+                <Cards data={ data } />
+                <Chart />
+                <CountryPicker />
+            </div>
+        )
+    }
 }
-
-export default App;
